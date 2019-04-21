f644c2362ff1a24d5586294e1b9b8a3823483232
Noah <noah@coronasoftware.net>
2019-04-21 13:50:20 -0700

    latest

--- dnd_maps/main.py
+++ dnd_maps/main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
+import numpy as np
 
 from pathlib import Path
 from PIL import Image, ImageTk
 from enum import Enum
 from more_itertools import chunked
+from itertools import chain
 
 image_dir = Path(__file__).parent.with_name('res') / 'images'
 
@@ -13,23 +15,6 @@ class ImageDim(Enum):
     sample_128 = 128, 128
 
 
-class ImageViewer(tk.Tk):
-
-    def __init__(self, img: Image, *args, **kwds):
-        super().__init__(*args, **kwds)
-
-
-        self.img = ImageTk.PhotoImage()
-        self.canvas = tk.Canvas(
-            self, width=self.img.width(), height=self.img.height()
-        )
-        self.canvas.pack(fill='both')
-        self.after_idle(self.view)
-
-    def view(self):
-        self.canvas.create_image(0, 0, anchor='nw', image=self.img)
-        self.update()
-
 def getall_images():
     return list(image_dir.rglob('*.png'))
 
@@ -38,24 +23,62 @@ def view(img: Path):
     viewer = ImageViewer(img)
     viewer.mainloop()
 
+
+def get_square(xfrom, xto, yfrom, yto, data):
+    out = []
+    for row in data[yfrom:yto]:
+        out.append(row[xfrom:xto])
+    return out
+
+
 def main():
     images = {fp.stem: fp for fp in getall_images()}
     fp = images.get('sample_1')
     dim = ImageDim['sample_128']
 
+    #TODO: Border Offset (this area is ignored)
+    borders = [0,0,0,0]
+
     img = Image.open(fp)
+    imgdata = chunked(img.getdata(), 128)
+    columns = (img.width - borders[0] - borders[1]) / 128
+    rows = (img.width - borders[2] - borders[3]) / 128
 
-    chunks = chunked(img.getdata(), 128)
-    chunk = next(chunks)
-    ImageViewer(chunk).mainloop()
+    data = [col for col in chunked(imgdata, int(columns))]
+    square = get_square(0, 128, 0, 128, data)
+    square = list(chain(*square))
+    # new = Image.fromarray()
+    print(bytearray('foo', 'utf-8'))
+    # new.show()
 
+
+
+    # print(columns, rows)
+    # img.show()
+
+    # chunks = chunked(img.getdata(), 128)
+
+'''
+o o o o o o
+o x x x x x
+o x x x x x
+o x x x x x
+o x x x x x
+o x x x x x
+
+'''
 
 '''
 Get a square (128x128 segment) at index x
 Calculate segment centerpoint
     (technically can be done manually, but dynamic better)
 Place overlay.png in center location
+Tint overlay graphic (manuall or auto detection)
+    grab first row, get RGB value
+    tint overlay to match sampled RGB value.
+        IMPORTANT!!!! Do not modify alpha value.
 repeat for index x+1
 '''
-
-#
+#getchannel (setchannel)
+#paste
+#transform
